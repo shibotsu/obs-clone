8ab5c98cd0e6f2955320895f54ec15e997d68964
Ivan Rojnić <ivan.rojnic10@gmail.com>
2025-04-14 16:46:25 +0200

    solving problems with chat

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Message;
+
+
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, SerializesModels;
+
+    public $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    // Privatni kanal prema korisniku kojem poruka ide
+    public function broadcastOn()
+    {
+        return new \Illuminate\Broadcasting\PrivateChannel('chat.' . $this->message->receiver_id);
+    }
+
+    // Kako će izgledati podatak koji se šalje frontendu
+    public function broadcastWith()
+    {
+        return [
+            'id' => $this->message->id,
+            'sender_id' => $this->message->sender_id,
+            'receiver_id' => $this->message->receiver_id,
+            'message' => $this->message->message,
+            'created_at' => $this->message->created_at->toDateTimeString(),
+        ];
+    }
+}
---
+++ config/broadcasting.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'default' => env('BROADCAST_DRIVER', 'pusher'),
+
+    'connections' => [
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
+            ],
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+    ],
+
+    'prefix' => env('BROADCAST_PREFIX', ''),
+
+];
