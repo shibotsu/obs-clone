407aada2c09096c2f3b56488dbcff8026e9ea99b
Mateo Štrok <mstrok@pop-os.home>
2025-06-02 23:42:41 +0200

    Add video.js package, fe-stream connection

--- src/pages/watch-page/WatchPage.jsx
+++ src/pages/watch-page/WatchPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "../home/Home.css";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
@@ -6,10 +6,14 @@ import { HomeSideBar } from "../../components/sidebar/HomeSidebar";
 import StreamHeader from "../../components/channel-description/StreamHeader";
 import StreamDescription from "../../components/channel-description/StreamDescription";
 import StreamChat from "../../components/chat/StreamChat";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
 import "./Watch.css";
 
 export default function WatchPage() {
   const { id } = useParams();
+  const videoNode = useRef(null);
+  const playerRef = useRef(null);
 
   const {
     data: channel,
@@ -25,6 +29,31 @@ export default function WatchPage() {
         return res.json();
       }),
   });
+
+  useEffect(() => {
+    if (!playerRef.current && videoNode.current) {
+      playerRef.current = videojs(videoNode.current, {
+        autoplay: true,
+        controls: true,
+        muted: true,
+        preload: "auto",
+        fluid: true,
+        sources: [
+          {
+            src: "http://192.168.1.96:8080/hls/mykey.m3u8",
+            type: "application/x-mpegURL",
+          },
+        ],
+      });
+    }
+
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, []);
 
   return (
     <div style={{ height: "calc(100vh - 65px)", overflow: "hidden" }}>
@@ -52,17 +81,18 @@ export default function WatchPage() {
             overflowY: "auto",
           }}
         >
-          <video
-            src="" // Replace with actual stream source or preview
-            controls
-            autoPlay
-            muted
-            style={{
-              aspectRatio: "16 / 9",
-              width: "100%",
-              objectFit: "cover",
-            }}
-          />
+          <div data-vjs-player>
+            <video
+              ref={videoNode}
+              className="video-js vjs-default-skin"
+              style={{
+                aspectRatio: "16 / 9",
+                width: "100%",
+                objectFit: "cover",
+              }}
+            ></video>
+          </div>
+
           {isLoading && <p>Loading stream info...</p>}
           {isError && <p>Streamer not found.</p>}
           {channel && (
